986af3a37336dc21d84dcb419668e166cf046ae0
chenyx113 <chenyouxin113@qq.com>
2025-01-23 19:06:28 +0800

    Update extract_onnx.py

--- tools/onnx-subgraph/extract_onnx.py
+++ tools/onnx-subgraph/extract_onnx.py
@@ -17,5 +17,5 @@ import torch
 import onnx
 import re
 print("python executed")
-extract_onnx_lib.split_onnx_ios('subgraphs_ios.txt','net/vit_large_simplify.onnx')
+extract_onnx_lib.split_onnx_ios('./subgraphs_ios.txt','./resnet-test.onnx')
 
