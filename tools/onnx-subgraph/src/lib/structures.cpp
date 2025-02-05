d049243b383cc8f8ac147ff46e09d77b0be82fe1
chenyx113 <chenyouxin113@qq.com>
2025-02-05 17:24:34 +0800

    Update structures.cpp

--- tools/onnx-subgraph/src/lib/structures.cpp
+++ tools/onnx-subgraph/src/lib/structures.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include "partition.h"
 #include <algorithm>
 int DetermineStructure(const onnx::GraphProto &graph, Device &d, PartitionStrategy strategy)
