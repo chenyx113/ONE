44862c30b564e3fa92064693e5a53427a1ca3e94
chenyx113 <chenyouxin113@qq.com>
2025-02-05 17:06:08 +0800

    Update onnx_subgraph_ut.py

--- tools/onnx-subgraph/onnx_subgraph_ut.py
+++ tools/onnx-subgraph/onnx_subgraph_ut.py
@@ -1,13 +1,17 @@
+# Copyright (c) 2025 Samsung Electronics Co., Ltd. All Rights Reserved
+
 import unittest
 import os
 import sys
 import extract_onnx_lib
 import shutil
 
+
 def onnx_parser_test(args):
     #exe = './onnx-subgraph ' + '--onnx=test.onnx'
     exe = './onnx-subgraph ' + args
     rec = os.system(exe)
+
 
 class ONNX_Parser_Test(unittest.TestCase):
     def test_parse_result_exception(self):
@@ -22,7 +26,7 @@ class ONNX_Parser_Test(unittest.TestCase):
         ret = os.path.exists('./subgraphs_ios.txt')
         if ret:
             os.remove('./subgraphs_ios.txt')
-            
+
         onnx_parser_test('--onnx=test.onnx')
         ret = os.path.exists('./subgraphs_ios.txt')
         self.assertEqual(ret, True)
@@ -32,10 +36,10 @@ class ONNX_Parser_Test(unittest.TestCase):
         if ret:
             shutil.rmtree(path='./subgraphs')
 
-        extract_onnx_lib.split_onnx_ios('./subgraphs_ios.txt','./test.onnx')
+        extract_onnx_lib.split_onnx_ios('./subgraphs_ios.txt', './test.onnx')
         ret = os.path.exists('./subgraphs')
         self.assertEqual(ret, True)
-        
+
         ret = os.path.exists('./subgraphs/CPU')
         self.assertEqual(ret, True)
 
@@ -45,7 +49,7 @@ class ONNX_Parser_Test(unittest.TestCase):
         ret = os.path.exists('./subgraphs/CPU/CPUsubgraph15.onnx')
         self.assertEqual(ret, True)
 
-        ret = os.path.exists('./subgraphs/NPU/NPUsubgraph15.onnx') 
+        ret = os.path.exists('./subgraphs/NPU/NPUsubgraph15.onnx')
         self.assertEqual(ret, True)
 
     def test_subgraph_exception(self):
@@ -53,7 +57,7 @@ class ONNX_Parser_Test(unittest.TestCase):
         if ret:
             shutil.rmtree(path='./subgraphs')
 
-        extract_onnx_lib.split_onnx_ios('./subgraphs_ios.txt','./fake.onnx')
+        extract_onnx_lib.split_onnx_ios('./subgraphs_ios.txt', './fake.onnx')
         ret = os.path.exists('./subgraphs')
         self.assertEqual(ret, False)
 
